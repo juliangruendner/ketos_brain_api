48d9a0e16ed870e0cdd4cc0b8261c37a17074ad9
Nicolas Wolf <nicolas.wolf@fau.de>
2017-11-10 15:56:18 +0100

    up

--- src/dockerAPI.py
+++ src/dockerAPI.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.userResource import UserList, User
-from rdb.rdb import connect_to_db
+from rdb.rdb import connect_to_db, create_all
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,5 +11,6 @@ api.add_resource(User, '/user/<int:user_id>', endpoint='user')
 
 if __name__ == '__main__':
     connect_to_db(app, 'postgresql://mad:MAD@0.0.0.0:5432/mad')
+    create_all()
     # set debug false in production mode
     app.run(debug=True, host='0.0.0.0', port=5000)
