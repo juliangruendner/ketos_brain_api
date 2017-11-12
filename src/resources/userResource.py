cf996a356c182088a4f5c74ad7d783bcb5927f5e
Nicolas Wolf <nicolas.wolf@fau.de>
2017-11-12 20:26:02 +0100

    up

--- src/resources/userResource.py
+++ src/resources/userResource.py
@@ -1,4 +1,4 @@
-from flask import g
+from flask import Flask, abort, request, jsonify, g, url_for
 from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
 from rdb.models.user import User
 from rdb.rdb import db
