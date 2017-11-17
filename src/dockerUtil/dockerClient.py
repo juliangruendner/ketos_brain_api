6ed54374f8c77b74974152f9eda4a002d2ff58d3
Nicolas Wolf <nicolas.wolf@fau.de>
2017-11-17 17:00:38 +0100

    fix

--- src/dockerUtil/dockerClient.py
+++ src/dockerUtil/dockerClient.py
@@ -3,7 +3,9 @@ import docker
 
 dockerClient = docker.from_env()
 
-username = os.environ.get('DOCKER_USERNAME')
-password = os.environ.get('DOCKER_PASSWORD')
-registry = os.environ.get('DOCKER_REGISTRY')
-dockerClient.login(username=username, password=password, registry=registry)
+# TODO: certificate for registry...
+
+# username = os.environ.get('DOCKER_USERNAME')
+# password = os.environ.get('DOCKER_PASSWORD')
+# registry = os.environ.get('DOCKER_REGISTRY')
+# dockerClient.login(username=username, password=password, registry=registry)
