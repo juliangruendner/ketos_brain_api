26d2f74a9e6cfa0d1062bf4c438d908c42860a48
Nicolas Wolf <nicolas.wolf@fau.de>
2017-11-20 17:34:19 +0100

    connect the private docker registry

--- src/dockerUtil/dockerClient.py
+++ src/dockerUtil/dockerClient.py
@@ -3,8 +3,6 @@ import docker
 
 dockerClient = docker.from_env()
 
-# TODO: certificate for registry...
-
 username = os.environ.get('DOCKER_USERNAME')
 password = os.environ.get('DOCKER_PASSWORD')
 registry = os.environ.get('DOCKER_REGISTRY')
