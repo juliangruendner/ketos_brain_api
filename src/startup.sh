2d6dc23bdfb09201ad3cb6b76a3d2420e89a5f8b
Nicolas Wolf <nicolas.wolf@fau.de>
2017-12-05 12:45:50 +0100

    up

--- src/startup.sh
+++ src/startup.sh
@@ -2,5 +2,5 @@ pip3 install -r requirements.txt
 chmod +x wait-for-it.sh
 ./wait-for-it.sh db:5432 --timeout=0
 # python3 mlServiceAPI.py
-uwsgi --ini mlServiceApi-uwsgi.ini
+sudo uwsgi --ini mlServiceApi-uwsgi.ini
 # uwsgi --http 0.0.0.0:5000 --processes 4 --threads 4 --mount /root/src=mlServiceAPI:app --manage-script-name --wsgi-file mlServiceAPI.py --callable app --master --plugin python3
