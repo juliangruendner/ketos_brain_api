23e3f39dd9c2eaa002185a6594b46fba7a5fa06f
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2018-01-13 15:22:38 +0100

    add logger

--- src/dockerUtil/dockerClient.py
+++ src/dockerUtil/dockerClient.py
@@ -1,11 +1,14 @@
 import docker
 import subprocess
 import config
+import logging
+logger = logging.getLogger(__name__)
 
-dockerClient = docker.from_env()
-
-dockerClient.login(username=config.DOCKER_REGISTRY_USERNAME, password=config.DOCKER_REGISTRY_PASSWORD, registry=config.DOCKER_REGISTRY_URL)
-
+try:
+    dockerClient = docker.from_env()
+    dockerClient.login(username=config.DOCKER_REGISTRY_USERNAME, password=config.DOCKER_REGISTRY_PASSWORD, registry=config.DOCKER_REGISTRY_URL)
+except Exception as e:
+    logger.error("Error when connecting to docker registry: ", exc_info=True)
 
 def wait_for_it(host, port, timeout=0):
     timeout_str = ""
--- src/mlServiceAPI.py
+++ src/mlServiceAPI.py
@@ -1,3 +1,5 @@
+import logging.config
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 from flask import Flask
 from flask_restful_swagger_2 import Api
 from resources.userResource import UserListResource, UserResource, UserLoginResource
