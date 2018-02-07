0781fe2cf89134e121887a454e4e1de83aec8e65
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2018-02-07 13:43:32 +0100

    remove superflous resource config requests

--- src/resources/resourceConfigResource.py
+++ src/resources/resourceConfigResource.py
@@ -8,10 +8,6 @@ DATA_PRE_RESOURCE_CONFIG_URL = "http://" + config.DATA_PREPROCESSING_HOST + "/re
 
 class ResourceConfigList(Resource):
     def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('resource_name', type = str, location = 'json')
-        self.parser.add_argument('resource_mapping', type = dict, action = 'append', location = 'json')
-
         super(ResourceConfigList, self).__init__()
     
     @auth.login_required
@@ -19,26 +15,6 @@ class ResourceConfigList(Resource):
         resp = requests.get(DATA_PRE_RESOURCE_CONFIG_URL).json()
 
         return resp, 200
-
-    @auth.login_required
-    def post(self):
-        args = self.parser.parse_args()
-        resource_name = args["resource_name"]
-        resource_mapping = args["resource_mapping"]
-
-        preprocess_body = {"resource_name": resource_name, "resource_mapping": resource_mapping}
-        resp = requests.post(DATA_PRE_RESOURCE_CONFIG_URL, json = preprocess_body).json()
-
-        return resp, 200
-
-    @auth.login_required
-    def delete(self):
-        args = self.parser.parse_args()
-        resource_name = args["resource_name"]
-
-        resp = requests.delete(DATA_PRE_RESOURCE_CONFIG_URL + "/" + resource_name).json()
-
-        return resp["resource_name"], 200
 
 class ResourceConfig(Resource):
     def __init__(self):
