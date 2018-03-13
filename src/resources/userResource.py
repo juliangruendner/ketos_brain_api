42dfbcd00df23713bf4b4d59a0cee4c7d1f53b1c
Nicolas Wolf <nicolas.wolf@fau.de>
2018-03-13 18:16:09 +0100

    full documentation for user

--- src/resources/userResource.py
+++ src/resources/userResource.py
@@ -27,15 +27,17 @@ class UserLoginResource(Resource):
         return g.user, 200
 
 
+user_post_parser = reqparse.RequestParser()
+user_post_parser.add_argument('first_name', type=str, location='json')
+user_post_parser.add_argument('last_name', type=str, location='json')
+user_post_parser.add_argument('username', type=str, required=True, help='No username provided', location='json')
+user_post_parser.add_argument('email', type=str, required=True, help='No email provided', location='json')
+user_post_parser.add_argument('password', type=str, required=True, help='No password provided', location='json')
+
+
 class UserListResource(Resource):
     def __init__(self):
         super(UserListResource, self).__init__()
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('first_name', type=str, location='json')
-        self.parser.add_argument('last_name', type=str, location='json')
-        self.parser.add_argument('username', type=str, required=True, help='No username provided', location='json')
-        self.parser.add_argument('email', type=str, required=True, help='No email provided', location='json')
-        self.parser.add_argument('password', type=str, required=True, help='No password provided', location='json')
 
     @auth.login_required
     @marshal_with(user_fields)
@@ -49,7 +51,7 @@ class UserListResource(Resource):
         "description": 'Get all the registered users for the current Ketos instance',
         "responses": {
             "200": {
-                "description": "users"
+                "description": "Returns the list of registered users"
             }
         }
     })
@@ -58,31 +60,98 @@ class UserListResource(Resource):
 
     @auth.login_required
     @marshal_with(user_fields)
+    @swagger.doc({
+        "summary": "Create a new user",
+        "tags": ["users"],
+        "produces": [
+            "application/json"
+        ],
+        "description": 'Option to create a new user by admin',
+        'reqparser': {'name': 'user post parser',
+                      'parser': user_post_parser},
+        "responses": {
+            "200": {
+                "description": "Returns newly created user"
+            }
+        }
+    })
     def post(self):
-        args = self.parser.parse_args()
+        args = user_post_parser.parse_args()
 
         u = User.create(username=args['username'], email=args['email'], password=args['password'], first_name=args['first_name'], last_name=args['last_name'])
 
         return u, 201
 
 
+user_put_parser = reqparse.RequestParser()
+user_put_parser.add_argument('first_name', type=str, location='json')
+user_put_parser.add_argument('last_name', type=str, location='json')
+user_put_parser.add_argument('username', type=str, location='json')
+user_put_parser.add_argument('email', type=str, location='json')
+user_put_parser.add_argument('password', type=str, location='json')
+
+
 class UserResource(Resource):
     def __init__(self):
         super(UserResource, self).__init__()
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('first_name', type=str, location='json')
-        self.parser.add_argument('last_name', type=str, location='json')
-        self.parser.add_argument('username', type=str, location='json')
-        self.parser.add_argument('email', type=str, location='json')
-        self.parser.add_argument('password', type=str, location='json')
 
     @auth.login_required
     @marshal_with(user_fields)
+    @swagger.doc({
+        "summary": "Gets a specific user",
+        "tags": ["users"],
+        "produces": [
+            "application/json"
+        ],
+        "description": 'Get the user for the given ID',
+        "responses": {
+            "200": {
+                "description": "Returns the user with the given ID"
+            },
+            "404": {
+                "description": "Not found error when user doesn't exist"
+            }
+        },
+        "parameters": [
+            {
+                "name": "user_id",
+                "in": "path",
+                "type": "integer",
+                "description": "The ID of the user",
+                "required": True
+            }
+        ],
+    })
     def get(self, user_id):
         return User.get(user_id), 200
 
     @auth.login_required
     @marshal_with(id_fields)
+    @swagger.doc({
+        "summary": "Deletes a specific user",
+        "tags": ["users"],
+        "produces": [
+            "application/json"
+        ],
+        "description": 'Delete the user for the given ID',
+        "responses": {
+            "200": {
+                "description": "Success: Returns the user the given ID"
+            },
+            "404": {
+                "description": "Not found error when user doesn't exist"
+            }
+        },
+        "parameters": [
+            {
+                "name": "user_id",
+                "in": "path",
+                "type": "integer",
+                "description": "The ID of the user",
+                "required": True
+            }
+        ],
+    })
     def delete(self, user_id):
         id = ID()
         id.id = User.delete(user_id)
@@ -91,8 +160,57 @@ class UserResource(Resource):
 
     @auth.login_required
     @marshal_with(user_fields)
+    @swagger.doc({
+        "summary": "Update a user",
+        "tags": ["users"],
+        "produces": [
+            "application/json"
+        ],
+        "description": 'Update a specific user',
+        "responses": {
+            "200": {
+                "description": "Success: Newly updated user is returned"
+            },
+            "404": {
+                "description": "Not found error when user doesn't exist"
+            }
+        },
+        "parameters": [
+            {
+                "name": "user_id",
+                "in": "path",
+                "type": "integer",
+                "description": "The ID of the user",
+                "required": True
+            },
+            {
+                "name": "user",
+                "in": "body",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "first_name": {
+                            "type": "string"
+                        },
+                        "last_name": {
+                            "type": "string"
+                        },
+                        "username": {
+                            "type": "string"
+                        },
+                        "email": {
+                            "type": "string"
+                        },
+                        "password": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        ],
+    })
     def put(self, user_id):
-        args = self.parser.parse_args()
+        args = user_put_parser.parse_args()
 
         u = User.update(user_id=user_id, username=args['username'], email=args['email'], password=args['password'], first_name=args['first_name'], last_name=args['last_name'])
 
